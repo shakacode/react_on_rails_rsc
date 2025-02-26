5453eaa574cd6ad54698b0f2ad8c1c44985e169d
Abanoub Ghadban <ghdbana@gmail.com>
2025-02-26 15:27:45 +0200

    Make function options optional in client type definitions

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,12 @@
-// @ts-expect-error Untyped module
 import {
   createFromFetch as originalCreateFromFetch,
   createFromReadableStream as originalCreateFromReadableStream,
+// @ts-expect-error Untyped module
 } from 'react-server-dom-webpack/client';
 
 // we don't care about options, so don't specify the type here
-export const createFromFetch: <T>(res: Promise<Response>, options: {}) => Promise<T> =
+export const createFromFetch: <T>(res: Promise<Response>, options?: {}) => Promise<T> =
   originalCreateFromFetch;
 
-export const createFromReadableStream: <T>(stream: ReadableStream, options: {}) => Promise<T> =
+export const createFromReadableStream: <T>(stream: ReadableStream, options?: {}) => Promise<T> =
   originalCreateFromReadableStream;

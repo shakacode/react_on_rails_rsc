83402cd1ee175999baa4d35f5624b5f4eaf5ae68
Abanoub Ghadban <ghdbana@gmail.com>
2025-02-26 11:58:29 +0200

    Refactor React Server Components wrapper with explicit type exports

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,12 @@
-import { createFromFetch, createFromReadableStream } from 'react-server-dom-webpack/client';
+import {
+  createFromFetch as originalCreateFromFetch,
+  createFromReadableStream as originalCreateFromReadableStream,
+} from 'react-server-dom-webpack/client';
 
-export {
-  createFromFetch,
-  createFromReadableStream,
+export function createFromFetch<T>(res: Promise<Response>): Promise<T> {
+  return originalCreateFromFetch(res);
+}
+
+export function createFromReadableStream<T>(stream: ReadableStream): Promise<T> {
+  return originalCreateFromReadableStream(stream);
 }
